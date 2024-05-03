19c9a7b5277ab9526b5850ac2a60f91ace0bc28e
vladvoicux64 <77429938+vladvoicux64@users.noreply.github.com>
2024-05-03 19:33:19 +0300

    Added the employee class.

---
+++ include/employee.h
@@ -0,0 +1,31 @@
+//
+// Created by Voicu Ioan Vladut on 03.05.2024.
+//
+
+#ifndef CSMS_EMPLOYEE_H
+#define CSMS_EMPLOYEE_H
+
+
+#include <string>
+
+class employee {
+    std::string name_;
+    float commission_;
+    void set_commission_(float commission);
+protected:
+    float earnings_;
+    virtual void earn(unsigned value);
+public:
+    [[nodiscard]] float get_earnings() const;
+    [[nodiscard]] float get_commission() const;
+    employee(std::string name, float commission);
+};
+
+class manager : employee {
+    void earn(unsigned value) override;
+public:
+    explicit manager(std::string name);
+};
+
+
+#endif //CSMS_EMPLOYEE_H
---
+++ src/employee.cpp
@@ -0,0 +1,42 @@
+//
+// Created by Voicu Ioan Vladut on 03.05.2024.
+//
+
+#include "employee.h"
+
+#include <utility>
+
+float employee::get_earnings() const
+{
+    return this->earnings_;
+}
+
+float employee::get_commission() const
+{
+    return this->commission_;
+}
+
+employee::employee(std::string name, float commission) : name_(std::move(name)), earnings_(0), commission_(commission)
+{
+
+}
+
+void employee::set_commission_(float commission)
+{
+    this->commission_ = commission;
+}
+
+void employee::earn(unsigned int value)
+{
+    this->earnings_ += (float)value * this->commission_;
+}
+
+manager::manager(std::string name) : employee(std::move(name), 0)
+{
+
+}
+
+void manager::earn(unsigned int value)
+{
+    this->earnings_ += (float)value;
+}
